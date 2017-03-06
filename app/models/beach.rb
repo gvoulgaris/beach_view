922e45a95bf358a4251614b248f270e022015b37
Raghu Betina <raghu@firstdraft.com>
2017-03-06 05:16:43 +0000

    Direct association between beaches and comments

--- app/models/beach.rb
+++ app/models/beach.rb
@@ -1,6 +1,9 @@
 class Beach < ApplicationRecord
   # Direct associations
 
+  has_many   :comments,
+             :dependent => :destroy
+
   has_many   :photos,
              :dependent => :destroy
 
