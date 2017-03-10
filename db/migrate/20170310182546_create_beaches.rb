bcc3b371e63150981535c104cb8ab432e17d15e2
Raghu Betina <raghu@firstdraft.com>
2017-03-10 18:26:53 +0000

    3rd draft

--- db/migrate/20170310182546_create_beaches.rb
+++ db/migrate/20170310182546_create_beaches.rb
@@ -3,12 +3,9 @@ class CreateBeaches < ActiveRecord::Migration
     create_table :beaches do |t|
       t.string :beach_name
       t.string :location
-      t.string :weather
-      t.time :sunset
-      t.integer :tides
-      t.integer :wave_height
       t.string :entrance_fee
       t.integer :umbrella_fee
+      t.string :available_activities_with_fees
       t.string :terrain
       t.string :terrain_color
       t.string :water_type
